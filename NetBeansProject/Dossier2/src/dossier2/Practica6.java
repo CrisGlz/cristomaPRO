84a5b2ea5930d9914003efad06f0791fed045428
Cristo Manuel Glez Delgado <xrizglz@gmail.com>
2022-10-04 00:43:54 +0100

    Realizado Practica 6 de Java.

---
+++ NetBeansProject/Dossier2/src/dossier2/Practica6.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dossier2;
+
+/**
+ *
+ * @author Cristo Manuel Glez Delgado <xrizglz@gmail.com>
+ */
+public class Practica6 {
+    public static void main(String[] args) {
+    System.out.println("\nLunes.\tMartes.\tMiérc.\tJueves.\tViernes.");
+    System.out.println("|=====\t=======\t=====\t======\t========|");
+    System.out.println("|PRO\tENT\tBASE\tPRO\tENT     |");
+    System.out.println("|PRO\tBASE\tPRO\tPRO\tBASE    |");    
+    System.out.println("|LEN\tING\tENT\tFOL\tFOL     |");
+    System.out.println("|---------------------------------------|");
+    System.out.println("|DESC\tDESC\tDESC\tDESC\tDESC    |");
+    System.out.println("|---------------------------------------|");
+    System.out.println("|SIS\tING\tSIS\tSIS\tLEN     |");
+    System.out.println("|SIS\tFOL\tSIS\tBASE\tPRO     |");
+    System.out.println("|BASE\tLEN\tLEN\tBASE\tPRO     |");
+    System.out.println("|======\t=======\t=====\t======\t========|");
+    }
+   
+}
