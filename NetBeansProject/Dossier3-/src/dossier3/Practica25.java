f0aea921a24eb4ef9b081e6fa74886e634cf5503
CrisGlz <xrizglz@gmail.com>
2022-11-11 16:11:27 +0000

    Realizado ejercicio 25

---
+++ NetBeansProject/Dossier3-/src/dossier3/Practica25.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dossier3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cristo Manuel Glez Delgado <xrizglz@gmail.com>
+ */
+public class Practica25 {
+    
+    
+    public static int sumarValores(int array[], int posArray) {
+        int tam = posArray;
+        int rta;
+      
+        if (tam == 0) {
+            return array[tam];
+        } else {
+            rta = (array[tam]) + sumarValores(array, tam - 1);
+
+        }
+        return rta;
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Cuantos numeros quiere ingresar? en el Array");
+        int arr[] = new int[sc.nextInt()];
+
+        System.out.println("Que numeros quieres :");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(sumarValores(arr, arr.length - 1));
+    }
+    }
+    
+    
+
